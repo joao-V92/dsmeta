7ff966017eae606188c4ef8e671d1cd3b2fe4df5
joao-V92 <joao.dev92@gmail.com>
2022-07-19 22:31:40 -0300

    Update React 18.2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
@@ -11,14 +12,14 @@ function App() {
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-              <SalesCard />
+            <SalesCard />
           </div>
+
         </section>
       </main>
-
     </>
   )
+
 }
 
 export default App;
-
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,17 @@
 import logo from '../../assets/img/logo.svg';
-import './styles.css';
 
+import'./styles.css';
 
 function Header() {
     return (
+
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://instagram.com/devsuperior.ig"> @devsuperior.ig</a>
                 </p>
             </div>
         </header>
@@ -18,4 +19,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
